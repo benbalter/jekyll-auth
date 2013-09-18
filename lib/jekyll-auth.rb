89f923238d56e9fc622b28075333cb6370a9651a
Ben Balter <ben.balter@github.com>
2013-09-18 09:57:04 -1000

    abs path to includes

--- lib/jekyll-auth.rb
+++ lib/jekyll-auth.rb
@@ -2,9 +2,9 @@ require 'rubygems'
 require 'sinatra-index'
 require 'sinatra_auth_github'
 require 'rack'
-require 'jekyll-auth/version'
-require 'jekyll-auth/auth-site'
-require 'jekyll-auth/jekyll-site'
+require File.dirname(__FILE__) + '/jekyll-auth/version'
+require File.dirname(__FILE__) + '/jekyll-auth/auth-site'
+require File.dirname(__FILE__) + '/jekyll-auth/jekyll-site'
 
 class JekyllAuth
   def self.site
