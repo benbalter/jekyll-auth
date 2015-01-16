a579632b4fa7ae726282df97f9f01770d0ad6e9e
Ben Balter <ben.balter@github.com>
2015-01-16 15:16:16 -0500

    move config to main file

--- lib/jekyll-auth.rb
+++ lib/jekyll-auth.rb
@@ -5,7 +5,6 @@ require 'rack'
 require 'dotenv'
 require 'safe_yaml'
 require_relative 'jekyll-auth/version'
-require_relative 'jekyll-auth/config'
 require_relative 'jekyll-auth/auth-site'
 require_relative 'jekyll-auth/jekyll-site'
 Dotenv.load
@@ -16,5 +15,27 @@ class JekyllAuth
       use JekyllAuth::AuthSite
       run JekyllAuth::JekyllSite
     end
+  end
+  
+  def self.config_file
+    File.join(Dir.pwd, "_config.yml")
+  end
+
+  def self.config
+    @config ||= begin
+      config = YAML.safe_load_file(config_file)
+      config["jekyll_auth"]
+    rescue
+      {}
+    end
+  end
+
+  def self.whitelist
+    whitelist = JekyllAuth::config["whitelist"]
+    Regexp.new(whitelist.join("|")) unless whitelist.nil?
+  end
+
+  def self.ssl?
+    !!JekyllAuth::config["ssl"]
   end
 end
