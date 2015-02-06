f76a151db734fa8d87a3a9794406b3ae8765ee20
Ben Balter <ben.balter@github.com>
2015-02-06 15:44:03 -0500

    fix for config load order

--- lib/jekyll-auth.rb
+++ lib/jekyll-auth.rb
@@ -6,6 +6,7 @@ require 'colorator'
 require 'mkmf'
 require_relative 'jekyll_auth/version'
 require_relative 'jekyll_auth/helpers'
+require_relative 'jekyll_auth/config'
 require_relative 'jekyll_auth/auth_site'
 require_relative 'jekyll_auth/jekyll_site'
 require_relative 'jekyll_auth/config_error'
@@ -19,27 +20,5 @@ class JekyllAuth
       use JekyllAuth::AuthSite
       run JekyllAuth::JekyllSite
     end
-  end
-
-  def self.config_file
-    File.join(Dir.pwd, "_config.yml")
-  end
-
-  def self.config
-    @config ||= begin
-      config = YAML.safe_load_file(config_file)
-      config["jekyll_auth"] || {}
-    rescue
-      {}
-    end
-  end
-
-  def self.whitelist
-    whitelist = JekyllAuth::config["whitelist"]
-    Regexp.new(whitelist.join("|")) unless whitelist.nil?
-  end
-
-  def self.ssl?
-    !!JekyllAuth::config["ssl"]
   end
 end
