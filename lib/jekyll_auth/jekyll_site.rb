1875d33577731a6b90798fb82c090923e81ab7c0
arthur <arthur@24b6.net>
2016-07-22 14:31:51 -0400

    Uses the jekyll _config.yml file to determine the webroot path

--- lib/jekyll_auth/jekyll_site.rb
+++ lib/jekyll_auth/jekyll_site.rb
@@ -1,12 +1,13 @@
 class JekyllAuth
   class JekyllSite < Sinatra::Base
     register Sinatra::Index
-    set :public_folder, File.expand_path('_site', Dir.pwd)
+    destination = JekyllAuth.destination
+    set :public_folder, File.expand_path(destination, Dir.pwd)
     use_static_index 'index.html'
 
     not_found do
       status 404
-      four_oh_four = File.expand_path('_site/404.html', Dir.pwd)
+      four_oh_four = File.expand_path(destination + '/404.html', Dir.pwd)
       File.read(four_oh_four) if File.exist?(four_oh_four)
     end
   end
