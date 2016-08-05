d0609face23ce17194ee3e20f07143c9541702bd
Sebastien Saunier <seb@saunier.me>
2016-08-05 18:20:40 +0200

    Fix teams auth

--- lib/jekyll_auth/sinatra/auth/github.rb
+++ lib/jekyll_auth/sinatra/auth/github.rb
@@ -1,10 +1,12 @@
 module Sinatra
   module Auth
     module Github
-      # Like the native github_team_authenticate! but accepts an array of team ids
-      def github_teams_authenticate!(teams)
-        authenticate!
-        halt([401, "Unauthorized User"]) unless teams.any? { |team_id| github_team_access?(team_id) }
+      module Helpers
+        # Like the native github_team_authenticate! but accepts an array of team ids
+        def github_teams_authenticate!(teams)
+          authenticate!
+          halt([401, "Unauthorized User"]) unless teams.any? { |team_id| github_team_access?(team_id) }
+        end
       end
     end
   end
